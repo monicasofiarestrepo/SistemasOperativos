7d3b102dc86c1e37cb489b663df5ab20069fd347
Mónica Restrepo L <65642162+monicasofiarestrepo@users.noreply.github.com>
2023-08-10 09:51:28 -0500

    Add files via upload

---
+++ ErsteÛbung/Punto12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  long double tarjeta;
+  char nombre[] = "Hola";
+  double contraseña; 
+  printf("ingrese su nombre,tarjeta de credito y contraseña separados por un espacio");
+  scanf("%Lf%s", &tarjeta, &nombre[], &contraseña);
+  scanf("%lf",&contraseña); 
+
+  printf("Su nombre: %Lf\n ")
+
+  return 0;
+ //sizeof imprime el tamaño en bytes de las variables. 
+
+}
